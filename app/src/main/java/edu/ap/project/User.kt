9bbc1526a91235615176904d6bb13071d1ab4611
Jureeke <vincent.jure@gmail.com>
2024-11-13 15:28:05 +0100

    Update User.kt

--- app/src/main/java/edu/ap/project/User.kt
+++ app/src/main/java/edu/ap/project/User.kt
@@ -4,7 +4,9 @@ data class User(
     val uid: String = "", // Firebase Auth UID
     val email: String = "",
     val username: String = "",
+    val profileImageUrl: String? = null,
+    val location: String? = null,
     val createdAt: Long = System.currentTimeMillis()
 ) {
-    constructor() : this("", "", "")
+    constructor() : this("", "", "", null, null)
 }
